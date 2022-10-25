bc635fb785099fdd8db8b5db79ba8a1b41e6322f
HeavyRain266 <ignacy423@gmail.com>
2022-10-25 17:18:51 +0200

    Restructure code & squash old commits
    Signed-off-by: HeavyRain266 <ignacy423@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,68 @@
-#[path = "../include/mappings.rs"] mod mappings;
+fn futhark_to_latin(rune: char) -> char {
+    match rune {
+        'ᚠ' => 'f',
+        'ᚢ' => 'u',
+        'ᚦ' => 'þ',
+        'ᚨ' => 'a',
+        'ᚱ' => 'r',
+        'ᚲ' => 'k',
+        'ᚷ' => 'g',
+        'ᚹ' => 'w',
+        'ᚺ' => 'h',
+        'ᚻ' => 'h',
+        'ᚾ' => 'n',
+        'ᛁ' => 'i',
+        'ᛃ' => 'j',
+        'ᛇ' => 'æ',
+        'ᛈ' => 'p',
+        'ᛉ' => 'z',
+        'ᛊ' => 's',
+        'ᛋ' => 's',
+        'ᛏ' => 't',
+        'ᛒ' => 'b',
+        'ᛖ' => 'e',
+        'ᛗ' => 'm',
+        'ᛚ' => 'l',
+        'ᛜ' => 'ŋ',
+        'ᛟ' => 'o',
+        'ᛞ' => 'd',
+        '-' => ' ',
 
-use mappings::{futhark_to_latin, latin_to_futhark};
+        rune => rune,
+    }
+}
+
+fn latin_to_futhark(script: char) -> char {
+    match script {
+        'f' => 'ᚠ',
+        'u' => 'ᚢ',
+        'þ' => 'ᚦ',
+        'a' => 'ᚨ',
+        'r' => 'ᚱ',
+        'k' => 'ᚲ',
+        'g' => 'ᚷ',
+        'w' => 'ᚹ',
+        'h' => 'ᚺ',
+        'n' => 'ᚾ',
+        'i' => 'ᛁ',
+        'j' => 'ᛃ',
+        'æ' => 'ᛇ',
+        'p' => 'ᛈ',
+        'z' => 'ᛉ',
+        's' => 'ᛊ',
+        't' => 'ᛏ',
+        'b' => 'ᛒ',
+        'e' => 'ᛖ',
+        'm' => 'ᛗ',
+        'l' => 'ᛚ',
+        'ŋ' => 'ᛜ',
+        'o' => 'ᛟ',
+        'd' => 'ᛞ',
+        '-' => ' ',
+
+        script => script,
+    }
+}
 
 fn main() {
     let mut args = std::env::args();
@@ -9,19 +71,33 @@ fn main() {
         match arg.as_str() {
             "-f" | "--futhark" => {
                 if let Some(futhark) = args.next() {
-                    println!("{}", futhark.chars().map(futhark_to_latin).collect::<String>())
+                    println!(
+                        "{}",
+                        futhark.chars().map(futhark_to_latin).collect::<String>()
+                    )
                 } else {
                     panic!("Add at least one argument");
                 }
             }
             "-l" | "--latin" => {
                 if let Some(latin) = args.next() {
-                    println!("{}", latin.chars().map(latin_to_futhark).collect::<String>())
+                    println!(
+                        "{}",
+                        latin.chars().map(latin_to_futhark).collect::<String>()
+                    )
                 } else {
                     panic!("Add at least one argument");
                 }
             }
             _ => {}
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test() {
+        todo!("Add test suite!")
     }
 }
